3b5834f08ee7e1bf0ac885c0710c49de912ea4f3
LuanaMB <luanabragaaa@hotmail.com>
2022-03-04 10:45:30 -0300

    add video mp4

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,13 +4,14 @@ import { Diretrizes } from '../../components/diretrizes';
 import { Doacoes } from '../../components/doacoes';
 import { Header } from '../../components/header/index';
 import { QuemSomos } from '../../components/quemSomos/index';
+import video from '../../assets/video.mp4';
 
 function Home() {
   return (
     <div>
       <Header>
         <video width="750" height="500" controls >
-          <source muted src="" type="video/mp4"/>
+          <source muted src={video} type="video/mp4"/>
         </video>
       </Header>
       <QuemSomos/>
