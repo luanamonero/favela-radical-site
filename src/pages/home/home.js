01754857ddc4a967bd0fc9bbbef59c0a2c4fd483
Luana Moneró <41486535+luanamonero@users.noreply.github.com>
2022-03-30 17:04:32 -0300

    Update home.js

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import { Atividades } from '../../components/atividades';
-import { Diretrizes } from '../../components/diretrizes';
-import { Doacoes } from '../../components/doacoes';
-import { Header } from '../../components/header/index';
-import { QuemSomos } from '../../components/quemSomos/index';
-import surf from '../../assets/video.mp4';
 
 function Home() {
   return (
