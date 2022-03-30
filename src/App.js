dbecabf53dc28f4dc42f8cc622acddf900c82363
Luana Moneró <41486535+luanamonero@users.noreply.github.com>
2022-03-30 17:09:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Switchs from './components/switch/switch';
-import { GlobalStyle } from '../src/styles/global';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Switchs/>
-      <GlobalStyle/>
     </BrowserRouter>
   );
 }
