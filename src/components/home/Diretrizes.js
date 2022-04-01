c9fca55381a5c675d02756c39393f5048fcd3941
LuanaMB <luanabragaaa@hotmail.com>
2022-03-31 23:07:59 -0300

    impl diretrizes

---
+++ src/components/home/Diretrizes.js
@@ -0,0 +1,40 @@
+import { Card, Col, Container, Row } from "react-bootstrap";
+import surf from "../../assets/surf.jpg";
+
+const Diretrizes = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <img className="imgDiretrizes" src={surf} alt="aula de surf" />
+        </Col>
+        <Col className="card-container">
+          <Card
+           border="danger" style={{ width: '25rem' }}
+          >
+            <Card.Body>
+              <Card.Title>Lorem ipsum dolor sit amet</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card
+           border="danger" style={{ width: '25rem' }}
+          >
+            <Card.Body>
+              <Card.Title>Lorem ipsum dolor sit amet</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Diretrizes;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import CarouselHeader from '../../components/Carrousel';
 import Header from '../../components/Header';
-import Letter from '../../components/home/letter';
-import QuemSomos from '../../components/home/quemSomos';
+import Diretrizes from '../../components/home/Diretrizes';
+import Letter from '../../components/home/Letter';
+import QuemSomos from '../../components/home/QuemSomos';
 
 
 function Home() {
@@ -13,6 +14,7 @@ function Home() {
       </Header>
       <QuemSomos/>
       <Letter/>
+      <Diretrizes />
     </div>
   );
 }
